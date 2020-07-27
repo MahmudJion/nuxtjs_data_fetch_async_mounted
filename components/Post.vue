93f3853aff886e6efc9ad7d9ec50599f8fabc902
quitus34 <43998247+Quintus34@users.noreply.github.com>
2020-07-27 18:54:14 +0600

    global head title init

---
+++ components/Post.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <b-card header="Popular Post">
+            <b-list-group>
+            <div v-for="(post,index) in popularPost" :key="index">
+                <b-list-group-item>
+                    <h3>{{ post.title }}</h3>
+                    <p>{{ post.body }}</p>
+                </b-list-group-item>
+            </div>
+        </b-list-group>
+        </b-card>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+    data () {
+		return {
+			popularPost: ''
+		};
+    },
+    mounted (){
+		axios.get(`https://jsonplaceholder.typicode.com/posts?_start=11&_limit=20`
+		).then((response) => {
+			this.popularPost = response.data
+		})
+	}
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <b-container>
+        <div>
+        <Navbar />
+        <b-card header="Latest Post"> 
+            <b-list-group>
+            <div v-for="(post,index) in latestPost" :key="index">
+                <b-list-group-item>
+                    <h3>{{ post.title }}</h3>
+                    <p>{{ post.body }}</p>
+                </b-list-group-item>
+            </div>
+        </b-list-group>
+        </b-card>
+        <Post/>
+        </div>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+    async asyncData ({ query, error }) {
+		let [latestPostResponse ] = await Promise.all([
+			axios.get(`https://jsonplaceholder.typicode.com/posts?_start=0&_limit=10`)
+		])
+		return{
+            latestPost: latestPostResponse.data
+		}
+	}
+}
+</script>
+
+<style>
+</style>
