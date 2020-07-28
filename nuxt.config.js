46603e7ee2db98f9532525db8a21279ee97569a7
MahmudJion <mahmudh.jion@yahoo.com>
2020-07-28 21:44:25 +0600

    Updated head meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: 'Nextjs data fetching example with axios in async and mounted' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
